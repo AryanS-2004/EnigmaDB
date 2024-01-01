5fcb572148031f49b437aa27ea59867d839daa46
AryanS-2004 <aryan.coding01@gmail.com>
2024-01-02 03:38:08 +0530

    Readme changed

--- README.md
+++ README.md
@@ -4,96 +4,62 @@ Enigma DB is a versatile, lightweight database built on top of PostgreSQL, trans
 
 ## Features
 
-- **Interactive CLI**: Enigma DB comes with a user-friendly Command Line Interface that allows users to easily interact with their PostgreSQL database using basic commands.
-
 - **Key-Value Storage**: Seamlessly utilize your PostgreSQL database as a key-value store. Enigma DB efficiently manages keys and their corresponding values, making it easy to store, retrieve, and manipulate data.
 
 - **Sharding for Performance**: Enigma DB intelligently shards the data across two PostgreSQL servers, optimizing performance by distributing keys based on their initial letter.
 
 - **CRON Job for Cleanup**: Keep your database clean and efficient with a CRON job that periodically deletes expired keys, ensuring optimal performance and resource utilization.
 
+- **Integration with Existing Database**: Easily integrate Enigma DB with your existing PostgreSQL database, turning it into a dynamic key-value store.
+
+
 ## Getting Started
 
 To get started with Enigma DB, follow these simple steps:
 
-1. **Installation**: Install the necessary dependencies and configure your PostgreSQL servers by providing their respective connection strings in the `env.ts` file.
-
-2. **Creating Tables**: Run the provided initialization script to create the required tables for Enigma DB.
-
-3. **Interactive Mode**: Launch Enigma DB in interactive mode to start issuing commands using the CLI. Supported commands include GET, SET, DELETE, and EXPIRE.
-
-4. **Integration with Existing Database**: Easily integrate Enigma DB with your existing PostgreSQL database, turning it into a dynamic key-value store.
-
-## Usage Flow
-
-Follow these steps to effectively use Enigma DB in your project:
-
-### 1. Configure PostgreSQL URLs
-
-Add the PostgreSQL URLs of your servers to the `env.ts` file.
-
-### 2. Compile TypeScript
-
-Run the TypeScript compiler to convert your TypeScript files to JavaScript.
+1. **Installation**: Install the necessary dependencies.
 
 ```bash
-tsc
+npm install enigma-db   
 ```
 
-### 3. Initialize Tables
+2. ** Configure PostgreSQL Servers
 
-Run the initialization script to create the required tables for Enigma DB.
+Start and Configure two PostgreSQL servers.
 
-```bash
-node dist/init.js
-```
 
-### 4. Run CRON Job
 
-Initiate the CRON job for periodic cleanup of expired keys.
-
-```bash
-node dist/cron.js
-
-```
-
-## Interactive Mode
-
-Launch Enigma DB in interactive mode to execute commands using the CLI.
-
-```bash
-node dist/interactive.js
-```
-## Normal Mode
+## Example
 
 Use Enigma DB in normal mode by incorporating it into your TypeScript/JavaScript code. Below is an example:
 
 
 ```typescript
-import Enigma from "./index";
+import Enigma from "enigma-db";
 
-const enigmaInstance = new Enigma();
+const enigmaInstance = new Enigma("postgresql://localhost:8000/test1", "postgresql://localhost:8001/test1");
 
-async function init(): Promise<void> {
-    await enigmaInstance.connect();
+async function init(): Promise<Error | void> {
+    try {
+        await enigmaInstance.connect();
+        let result1: number | Error = await enigmaInstance.set("key1", "val1", 3600);
+        console.log(result1);
+        for (let i = 0; i < 5; i++) {
+            let result2: string | Error = await enigmaInstance.get("key1");
+            console.log(result2);
+        }
+        let result3: number | Error = await enigmaInstance.expire("key1", 1800);
+        console.log(result3);
 
-    // Perform key-value operations
-    let result = await enigmaInstance.set("key1", "val1", 3600);
-    console.log(result);
+        let result4: number | Error = await enigmaInstance.del("key1");
+        console.log(result4);
 
-    for (let i = 0; i < 5; i++) {
-        result = await enigmaInstance.get("key1");
-        console.log(result);
+    } catch (err: any) {
+        console.error(err);
+        // throw new Error(err);
+    } finally {
+        await enigmaInstance.disconnect();
     }
-
-    result = await enigmaInstance.expire("key1", 1800);
-    console.log(result);
-
-    result = await enigmaInstance.del("key1");
-    console.log(result);
-
-    await enigmaInstance.disconnect();
 }
-
 init();
 ```
---
+++ dist/src/index.js
@@ -0,0 +1,227 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const pg_1 = require("pg");
+class Enigma {
+    constructor(server1, server2) {
+        this.client1 = new pg_1.Client({
+            connectionString: server1,
+        });
+        this.client2 = new pg_1.Client({
+            connectionString: server2,
+        });
+        this.isConnected = false;
+    }
+    connect() {
+        return __awaiter(this, void 0, void 0, function* () {
+            try {
+                yield this.client1.connect();
+                yield this.client2.connect();
+                const query = {
+                    text: "CREATE TABLE kv_store(key VARCHAR(255) PRIMARY KEY, val TEXT, expired_at INTEGER);",
+                };
+                yield this.client1.query("DROP TABLE IF EXISTS kv_store;");
+                yield this.client2.query("DROP TABLE IF EXISTS kv_store;");
+                yield this.client1.query(query);
+                yield this.client2.query(query);
+                this.isConnected = true;
+                // This job runs every 10 mins and does a batch deletion of the keys that are expired
+                // and due to this, the rebalancing of the tree is done only once every 30 mins
+                // as when the user deletes a key it is soft deleted not hard deleted
+                // this.delRows();
+                setInterval(this.delRows, 600000);
+            }
+            catch (err) {
+                throw new Error(`\n\nError during table creation: ${err}\n\n`);
+            }
+        });
+    }
+    delRows() {
+        return __awaiter(this, void 0, void 0, function* () {
+            //Current time in epoch seconds
+            const currTime = Math.floor(new Date().getTime() / 1000);
+            const query = {
+                text: "DELETE FROM kv_store WHERE expired_at <= $1",
+                values: [currTime],
+            };
+            try {
+                // Deleting keys with time to live less than the current time from DB1
+                yield this.client1.query(query);
+            }
+            catch (err) {
+                // throw new Error(`Error in deleting expired keys:  ${err.message}`);
+            }
+            try {
+                // Deleting keys with time to live less than the current time from DB2
+                yield this.client2.query(query);
+            }
+            catch (err) {
+                // throw new Error(`Error in deleting expired keys:  ${err.message}`);
+            }
+        });
+    }
+    checkConnection() {
+        if (!this.isConnected) {
+            throw new Error("Call connect() first.");
+        }
+    }
+    disconnect() {
+        return __awaiter(this, void 0, void 0, function* () {
+            this.checkConnection();
+            try {
+                // console.log("Disconnecting Clients...");
+                yield this.client1.end();
+                yield this.client2.end();
+                // console.log("Clients Disconnected!!\n");
+            }
+            catch (err) {
+                throw new Error(`\n\nError disconnecting: ${err}\n\n`);
+            }
+        });
+    }
+    get(key) {
+        return __awaiter(this, void 0, void 0, function* () {
+            this.checkConnection();
+            if (key) {
+                const query = {
+                    text: "SELECT val FROM kv_store WHERE key=$1 AND expired_at > EXTRACT(epoch FROM NOW());",
+                    values: [key],
+                };
+                try {
+                    const letter = key.charAt(0).toLowerCase();
+                    let res;
+                    if (letter <= "m") {
+                        res = yield this.client1.query(query);
+                    }
+                    else {
+                        res = yield this.client2.query(query);
+                    }
+                    if (res.rows.length > 0) {
+                        return res.rows[0].val;
+                    }
+                    else {
+                        throw new Error(`${key} not found!!\n`);
+                    }
+                }
+                catch (err) {
+                    throw new Error(`Error executing GET query: ${err.message}`);
+                }
+            }
+            else {
+                throw new Error("The passed parameter is incorrect. Please pass a correct parameter.");
+            }
+        });
+    }
+    set(key, val, ttl) {
+        return __awaiter(this, void 0, void 0, function* () {
+            this.checkConnection();
+            const currentEpochTime = Math.floor(new Date().getTime() / 1000);
+            ttl = ttl + currentEpochTime;
+            if (key && val && ttl) {
+                const query = {
+                    text: "INSERT INTO kv_store (key, val, expired_at) VALUES ($1, $2, $3) ON CONFLICT (key) DO UPDATE SET key = $1, val = $2, expired_at = $3;",
+                    values: [key, val, ttl],
+                };
+                try {
+                    const letter = key.charAt(0).toLowerCase();
+                    let res;
+                    if (letter <= "m") {
+                        res = yield this.client1.query(query);
+                    }
+                    else {
+                        res = yield this.client2.query(query);
+                    }
+                    if ((res === null || res === void 0 ? void 0 : res.rowCount) && (res === null || res === void 0 ? void 0 : res.rowCount) > 0) {
+                        return 1;
+                    }
+                    else {
+                        throw new Error(`${key} not found!!\n`);
+                    }
+                }
+                catch (err) {
+                    throw new Error(`Error executing SET query: ${err.message}`);
+                }
+            }
+            else {
+                throw new Error("The passed parameters are incorrect. Please pass correct parameters.");
+            }
+        });
+    }
+    del(key) {
+        return __awaiter(this, void 0, void 0, function* () {
+            this.checkConnection();
+            if (key) {
+                const query = {
+                    text: "UPDATE kv_store SET expired_at=-1 WHERE key=$1 AND expired_at > EXTRACT(epoch FROM NOW());",
+                    values: [key],
+                };
+                try {
+                    const letter = key.charAt(0).toLowerCase();
+                    let res;
+                    if (letter <= "m") {
+                        res = yield this.client1.query(query);
+                    }
+                    else {
+                        res = yield this.client2.query(query);
+                    }
+                    if ((res === null || res === void 0 ? void 0 : res.rowCount) && (res === null || res === void 0 ? void 0 : res.rowCount) > 0) {
+                        return 1;
+                    }
+                    else {
+                        throw new Error(`${key} not found!!\n`);
+                    }
+                }
+                catch (err) {
+                    throw new Error(`Error executing DELETE query: ${err.message}`);
+                }
+            }
+            else {
+                throw new Error("The passed parameter is incorrect. Please pass a correct parameter.");
+            }
+        });
+    }
+    expire(key, ttl) {
+        return __awaiter(this, void 0, void 0, function* () {
+            this.checkConnection();
+            const currentEpochTime = Math.floor(new Date().getTime() / 1000);
+            ttl = ttl + currentEpochTime;
+            if (key && ttl) {
+                const query = {
+                    text: "UPDATE kv_store SET expired_at=$1 WHERE key=$2;",
+                    values: [ttl, key],
+                };
+                try {
+                    const letter = key.charAt(0).toLowerCase();
+                    let res;
+                    if (letter <= "m") {
+                        res = yield this.client1.query(query);
+                    }
+                    else {
+                        res = yield this.client2.query(query);
+                    }
+                    if ((res === null || res === void 0 ? void 0 : res.rowCount) && (res === null || res === void 0 ? void 0 : res.rowCount) > 0) {
+                        return 1;
+                    }
+                    else {
+                        throw new Error(`${key} not found!!\n`);
+                    }
+                }
+                catch (err) {
+                    throw new Error(`Error executing EXPIRE query: ${err.message}`);
+                }
+            }
+            else {
+                throw new Error("The passed parameters are incorrect. Please pass correct parameters.");
+            }
+        });
+    }
+}
+exports.default = Enigma;
